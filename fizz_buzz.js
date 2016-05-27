d92a1c9db2f1f2afd5bebc1323bd3f127d8bc4b5
Sierra Gregg <sierragregg@Gmail.com>
2016-05-26 21:48:23 -0500

    Added JQuery to pull information from an HTML form

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
-	printNumbers('fizz', 'buzz', 100);
+	$('#preferences').submit(function() {
+		$('#printout').empty(); //Clear the contents of the section
+		printNumbers($('#divBy3Word').val(), $('#divBy5Word').val(), $('#numToPrint').val());
+		event.preventDefault();
+	});
 });
 
 var printNumbers = function(divisibleBy3, divisibleBy5, loopNum) {
-	
 	for(var i = 1; i <= loopNum; i++) {
-		if((i % 3 === 0) && (i % 5 === 0)) {
-			$("#printout").append('<p>' + divisibleBy3 + ' ' + divisibleBy5 + '</p>');
-		} else if(i % 3 === 0) {
+		if(!(i % 30) && !(i % 5)) {
+			$("#printout").append('<p>' + divisibleBy3 + divisibleBy5 + '</p>');
+		} else if(!(i % 3)) {
 			$("#printout").append('<p>' + divisibleBy3 + '</p>');
-		} else if(i % 5 === 0) {
+		} else if(!(i % 5)) {
 			$("#printout").append('<p>' + divisibleBy5 + '</p>');
 		} else {
 			$("#printout").append('<p>' + i + '</p>');
 		}
 	}
-
 }
