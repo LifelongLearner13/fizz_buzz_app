1db70b83cbc9552576364b145ef94a402d42aaf6
Sierra Gregg <sierragregg@Gmail.com>
2016-05-26 19:24:24 -0500

    Working but ugly version

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-
-	$("#printout").html("This is Hello World by JQuery");
-
+	printNumbers('fizz', 'buzz', 100);
 });
+
+var printNumbers = function(divisibleBy3, divisibleBy5, loopNum) {
+	
+	for(var i = 1; i <= loopNum; i++) {
+		if((i % 3 === 0) && (i % 5 === 0)) {
+			$("#printout").append('<p>' + divisibleBy3 + ' ' + divisibleBy5 + '</p>');
+		} else if(i % 3 === 0) {
+			$("#printout").append('<p>' + divisibleBy3 + '</p>');
+		} else if(i % 5 === 0) {
+			$("#printout").append('<p>' + divisibleBy5 + '</p>');
+		} else {
+			$("#printout").append('<p>' + i + '</p>');
+		}
+	}
+
+}
